eda493d987c9f03482cd539ae36d7bf5a08ed2a6
RachelPack <92027359+rachelpack@users.noreply.github.com>
2022-08-03 22:28:22 +0300

    Targil0

---
+++ Targil0/Program1538.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            welcome1538();
+            welcome3096();
+            Console.ReadKey();
+        }
+        static partial void welcome3096();
+        private static void welcome1538()
+        {
+            Console.WriteLine("hello");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+    }
+}
---
+++ Targil0/Program3096.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void welcome3096()
+        {
+                Console.WriteLine("i am also here");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
